44d57fac0a4f02f2350ee157097d4248e0b45b70
Nargis-S <nargissathik1605@gmail.com>
2025-07-24 10:19:27 +0530

    Assignment

---
+++ C2TC_COREJAVA8ASSIGNMENT/src/ASSIGNMENT2/CommissionDemo.java
@@ -0,0 +1,16 @@
+package ASSIGNMENT2;
+
+public class CommissionDemo {
+	    public static void main(String[] args) {
+	        // Create Student object
+	        Student student = new Student();
+
+	        // Create Commission object
+	        Commission employee = new Commission();
+
+	        // Accept details and calculate commission
+	        employee.acceptDetails();
+	        employee.calculateCommission();
+	    }
+	}
+
---
+++ C2TC_COREJAVA8ASSIGNMENT/src/ASSIGNMENT2/Student.java
@@ -0,0 +1,7 @@
+package ASSIGNMENT2;
+
+public class Student {
+	    public Student() {
+	        System.out.println("Student object is created");
+	    }
+	}
